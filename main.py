4f2b2b68022eabaf30af5111b23d89487f64a1ce
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-12-19 01:53:36 +0100

    use formatter

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import src.pddl as plarser
 import argparse
 
+
 def main():
-    args_parser = argparse.ArgumentParser(description="Start parsing Domain and Problem files with PDDL.jl using Python")
+    args_parser = argparse.ArgumentParser(
+        description="Start parsing Domain and Problem files with PDDL.jl using Python"
+    )
     args_parser.add_argument
-    args_parser.add_argument('domain', type=str, help='PDDL domain file')
-    args_parser.add_argument('problem', type=str, help='PDDL problem file')
-    args_parser.add_argument('v', '--verbose', help="Increases the output's verbosity")
+    args_parser.add_argument("domain", type=str, help="PDDL domain file")
+    args_parser.add_argument("problem", type=str, help="PDDL problem file")
+    args_parser.add_argument("v", "--verbose", help="Increases the output's verbosity")
     args = args_parser.parse_args()
     apla_tbx = plarser.AutomatedPlanning(args.domain, args.problem)
     print(apla_tbx.current_state)
+
 
 if __name__ == "__main__":
     main()
--- src/modules.py
+++ src/modules.py
@@ -3,41 +3,40 @@ import threading
 
 g_import_finished = False
 
+
 def __loading_bar():
     animation = [
-    "[        ]",
-    "[=       ]",
-    "[===     ]",
-    "[====    ]",
-    "[=====   ]",
-    "[======  ]",
-    "[======= ]",
-    "[========]",
-    "[ =======]",
-    "[  ======]",
-    "[   =====]",
-    "[    ====]",
-    "[     ===]",
-    "[      ==]",
-    "[       =]",
-    "[        ]",
-    "[        ]"
+        "[        ]",
+        "[=       ]",
+        "[===     ]",
+        "[====    ]",
+        "[=====   ]",
+        "[======  ]",
+        "[======= ]",
+        "[========]",
+        "[ =======]",
+        "[  ======]",
+        "[   =====]",
+        "[    ====]",
+        "[     ===]",
+        "[      ==]",
+        "[       =]",
+        "[        ]",
+        "[        ]",
     ]
 
     i = 0
     print("Getting Julia ready...")
     while not g_import_finished:
-        print(animation[i % len(animation)], end='\r')
-        time.sleep(.1)
+        print(animation[i % len(animation)], end="\r")
+        time.sleep(0.1)
         i += 1
 
 
 def loading_bar_handler(loading_is_finished):
     global loading_thread
     if not loading_is_finished:
-        loading_thread = threading.Thread(
-            target=__loading_bar
-        )
+        loading_thread = threading.Thread(target=__loading_bar)
         loading_thread.start()
     else:
         global g_import_finished
