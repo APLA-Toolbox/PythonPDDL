9a914f8cac4037ffaf3900fa690abb0b7f65dbd8
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-12-19 22:27:39 +0100

    add bfs

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def main():
     args_parser.add_argument
     args_parser.add_argument("domain", type=str, help="PDDL domain file")
     args_parser.add_argument("problem", type=str, help="PDDL problem file")
-    args_parser.add_argument("v", "--verbose", help="Increases the output's verbosity")
+    args_parser.add_argument("-v", "--verbose", help="Increases the output's verbosity")
     args = args_parser.parse_args()
     apla_tbx = plarser.AutomatedPlanning(args.domain, args.problem)
-    print(apla_tbx.current_state)
+    path = apla_tbx.get_actions_from_path(apla_tbx.breadth_first_search())
+    print(path)
 
 
 if __name__ == "__main__":
--- src/pddl.py
+++ src/pddl.py
@@ -1,4 +1,5 @@
 from .modules import loading_bar_handler
+from .state import State
 
 UI = False
 
@@ -10,7 +11,7 @@ _ = julia.Julia(compiled_modules=False)
 
 if UI:
     loading_bar_handler(True)
-    
+
 from julia import PDDL
 
 
@@ -19,14 +20,61 @@ class AutomatedPlanning:
         self.domain = PDDL.load_domain(domain_path)
         self.problem = PDDL.load_problem(problem_path)
         self.initial_state = PDDL.initialize(self.problem)
-        self.current_state = self.initial_state
 
-    def execute_action(self, action, state):
+    def __execute_action(self, action, state):
         return PDDL.execute(action, state)
 
-    def satisfies(self, asserted_state, state, domain):
-        return PDDL.satisfy(asserted_state, state, domain)[0]
+    def transition(self, state, action):
+        return PDDL.transition(self.domain, state, action, check=False)
+        
+    def available_actions(self, state):
+        return PDDL.available(state, self.domain)
 
+    def satisfies(self, asserted_state, state):
+        return PDDL.satisfy(asserted_state, state, self.domain)[0]
+
+    def __retrace_path(self, state):
+        path = []
+        while state.parent:
+            path.append(state)
+            state = state.parent
+        path.reverse()
+        return path
+
+    def get_actions_from_path(self, path):
+        actions = []
+        for state in path:
+            actions.append(state.parent_action)
+        return actions
+
+    def get_state_def_from_path(self, path):
+        trimmed_path = []
+        for state in path:
+            trimmed_path.append(state.description)
+        return trimmed_path
+
+    def breadth_first_search(self):
+        visited = []
+        init = State(self.initial_state)
+        queue = [init]
+        while queue:
+            current_state = queue.pop(0)
+            if current_state not in visited:
+                visited.append(current_state)
+
+                if self.satisfies(self.problem.goal, current_state.description):
+                    path = self.__retrace_path(current_state)
+                    return path
+                
+                actions = self.available_actions(current_state.description)
+                for act in actions:
+                    child = State(self.transition(current_state.description, act), act, current_state)
+                    if child in visited:
+                        continue
+                    queue.append(child)
+
+        print("-/!\- No path found -/!\-")
+        return []
 
 if __name__ == "__main__":
     ap = AutomatedPlanning("..data/domain.pddl", "..data/problem.pddl")
