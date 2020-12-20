ceb88205d26db8b58b747cc163b85cd09144b344
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-12-20 18:42:22 +0100

    fix tests

--- tests/test_pddl.py
+++ tests/test_pddl.py
@@ -6,22 +6,30 @@ from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
-def test_automated_planning_parsing():
-    import src.pddl
+def test_parsing():
+    from src.automated_planning import AutomatedPlanning
 
-    apla = src.pddl.AutomatedPlanning("data/domain.pddl", "data/problem.pddl")
+    apla = AutomatedPlanning("data/domain.pddl", "data/problem.pddl")
     assert str(apla.problem) != "" and str(apla.domain) != ""
 
 
-def test_automated_planning_states():
-    import src.pddl
+def test_available_actions():
+    from src.automated_planning import AutomatedPlanning
 
-    apla = src.pddl.AutomatedPlanning("data/domain.pddl", "data/problem.pddl")
-    assert str(apla.current_state) == str(apla.initial_state)
+    apla = AutomatedPlanning("data/domain.pddl", "data/problem.pddl")
+    actions = apla.available_actions(apla.initial_state)
+    assert(len(actions) > 0)
 
+def test_execute_action():
+    from src.automated_planning import AutomatedPlanning
 
-def test_automated_planning_state_assertion():
-    import src.pddl
+    apla = AutomatedPlanning("data/domain.pddl", "data/problem.pddl")
+    actions = apla.available_actions(apla.initial_state)
+    new_state = apla.transition(apla.initial_state, actions[0])
+    assert(str(new_state) != str(apla.initial_state))
 
-    apla = src.pddl.AutomatedPlanning("data/domain.pddl", "data/problem.pddl")
-    assert apla.satisfies(apla.problem.goal, apla.initial_state, apla.domain) == False
+def test_state_assertion():
+    from src.automated_planning import AutomatedPlanning
+
+    apla = AutomatedPlanning("data/domain.pddl", "data/problem.pddl")
+    assert apla.satisfies(apla.problem.goal, apla.initial_state) == False
