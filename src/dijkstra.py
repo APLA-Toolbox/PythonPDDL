00b6898bc2cdc658123ab680040e933e456aad73
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-12-20 19:23:28 +0100

    reformat with black

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -2,10 +2,16 @@ from .node import Node
 import math
 from .heuristics import zero_heuristic
 
-class DijkstraBestFirstSearch():
+
+class DijkstraBestFirstSearch:
     def __init__(self, automated_planner):
         self.automated_planner = automated_planner
-        self.init = Node(self.automated_planner.initial_state, automated_planner, is_closed=False, is_open=True)
+        self.init = Node(
+            self.automated_planner.initial_state,
+            automated_planner,
+            is_closed=False,
+            is_open=True,
+        )
         self.open_nodes_n = 1
         self.nodes = dict()
         self.nodes[self.__hash(self.init)] = self.init
@@ -17,19 +23,32 @@ class DijkstraBestFirstSearch():
 
     def search(self):
         while self.open_nodes_n > 0:
-            current_key = min([n for n in self.nodes if self.nodes[n].is_open], key=(lambda  k: self.nodes[k].f_cost))
+            current_key = min(
+                [n for n in self.nodes if self.nodes[n].is_open],
+                key=(lambda k: self.nodes[k].f_cost),
+            )
             current_node = self.nodes[current_key]
 
-            if self.automated_planner.satisfies(self.automated_planner.problem.goal, current_node.state):
-                    return current_node
+            if self.automated_planner.satisfies(
+                self.automated_planner.problem.goal, current_node.state
+            ):
+                return current_node
 
             current_node.is_closed = True
             current_node.is_open = False
             self.open_nodes_n -= 1
-            
+
             actions = self.automated_planner.available_actions(current_node.state)
             for act in actions:
-                child = Node(state=self.automated_planner.transition(current_node.state, act), automated_planner=self.automated_planner, parent_action=act, parent=current_node, heuristic=zero_heuristic, is_closed=False, is_open=True)
+                child = Node(
+                    state=self.automated_planner.transition(current_node.state, act),
+                    automated_planner=self.automated_planner,
+                    parent_action=act,
+                    parent=current_node,
+                    heuristic=zero_heuristic,
+                    is_closed=False,
+                    is_open=True,
+                )
                 child_hash = self.__hash(child)
                 if child_hash in self.nodes:
                     if self.nodes[child_hash].is_closed:
