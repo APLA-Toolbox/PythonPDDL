9a5045a44e35f289a2bb8ca161091ef6a6e210d8
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-12-20 14:41:11 +0100

    add empty dfs and astar class

---
+++ src/dfs.py
@@ -0,0 +1,7 @@
+class DepthFirstSearch():
+    def __init__(self, automated_planning):
+        self.automated_planning = automated_planning
+    
+    def search(self):
+        print("-/!\- No path found -/!\-")
+        return []
