58bfbbaed1980a19a070fb142ce6b51a2960290b
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-12-22 21:47:18 +0100

    change package name

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+    
 
 setuptools.setup(
-    name="pythonpddl-guilyx",  # Replace with your own username
-    version="0.2.0",
+    name="jupyddl",  # Replace with your own username
+    version="0.2.1",
     author="Erwin Lejeune",
     author_email="erwinlejeune.pro@gmail.com",
-    description="PythonPDDL is a PDDL planner built on top of a Julia parser",
+    description="Jupyddl is a PDDL planner built on top of a Julia parser",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/apla-toolbox/pythonpddl",
     packages=setuptools.find_packages(),
+    install_requires=required,
     classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: Ubuntu, MacOS",
+        "Framework :: Pytest",
     ],
     python_requires=">=3.6",
 )
