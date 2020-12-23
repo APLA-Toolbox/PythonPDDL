be261cf23a478224f2da1ad4bf1ace4e5c680903
guilyx <guilyx@users.noreply.github.com>
2020-12-23 00:24:11 +0000

    Apply formatting changes

--- jupyddl/__init__.py
+++ jupyddl/__init__.py
@@ -1,4 +1,4 @@
 from .automated_planner import AutomatedPlanner
 
 if __name__ == "__main__":
-    _ = AutomatedPlanner("data/domain.pddl", "data/problem.pddl")    
+    _ = AutomatedPlanner("data/domain.pddl", "data/problem.pddl")
