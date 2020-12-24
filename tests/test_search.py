7c9dda6805cb15ffcb5e0b96f31ba1a1af3c9c0b
Sampreet Sarkar <sampreets3@gmail.com>
2020-12-24 14:25:44 +0100

    Added tests for Dijkstra's and A*

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,3 +1,4 @@
+from jupyddl.heuristics import goal_count_heuristic, zero_heuristic
 from jupyddl.automated_planner import AutomatedPlanner
 from jupyddl.dijkstra import DijkstraBestFirstSearch
 from jupyddl.a_star import AStarBestFirstSearch
@@ -7,14 +8,26 @@ from os import path
 import coloredlogs
 import sys
 
+apla = AutomatedPlanner("data/domain.pddl", "data/problem.pddl")
+
 def test_searchDFS():
-    apla = AutomatedPlanner("data/domain.pddl", "data/problem.pddl")
     dfs = DepthFirstSearch(apla)
     _,ct = dfs.search()
     assert ct != 0
 
 def test_searchBFS():
-    apla = AutomatedPlanner("data/domain.pddl", "data/problem.pddl")
     bfs = BreadthFirstSearch(apla)
     _,ct = bfs.search()
     assert ct != 0
+
+def test_searchDijkstra():
+    dijk = DijkstraBestFirstSearch(apla)
+    res = dijk.search()                        # None, computation_time, opened_nodes(in this order)
+    assert(res[1] != 0)
+    assert(res[-1] >= 1)
+
+def test_searchAStar():
+    astar = AStarBestFirstSearch(apla, goal_count_heuristic)
+    res = astar.search()                        # None, computation_time, opened_nodes(in this order)
+    assert(res[1] != 0)
+    assert(res[-1] >= 1)
