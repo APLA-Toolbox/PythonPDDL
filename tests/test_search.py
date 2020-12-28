4ef0234addcafeefc2fcb8c517289fbe5c6b2716
guilyx <guilyx@users.noreply.github.com>
2020-12-28 07:42:26 +0000

    Apply formatting changes

--- tests/test_search.py
+++ tests/test_search.py
@@ -14,24 +14,24 @@ apla = AutomatedPlanner("data/domain.pddl", "data/problem.pddl")
 def test_searchDFS():
     dfs = DepthFirstSearch(apla)
     res = dfs.search()
-    assert res[1] != 0              # Path, computation time, opened nodes
+    assert res[1] != 0  # Path, computation time, opened nodes
 
 
 def test_searchBFS():
     bfs = BreadthFirstSearch(apla)
-    res = bfs.search()              # Path, computation time, opened nodes
+    res = bfs.search()  # Path, computation time, opened nodes
     assert res[1] != 0
 
 
 def test_searchDijkstra():
     dijk = DijkstraBestFirstSearch(apla)
-    res = dijk.search()             # Goal, computation_time, opened_nodes(in this order)
-    assert res[1] != 0              # Assert that it took some time to compute
-    assert res[-1] > 0              # Assert that it visited some nodes
+    res = dijk.search()  # Goal, computation_time, opened_nodes(in this order)
+    assert res[1] != 0  # Assert that it took some time to compute
+    assert res[-1] > 0  # Assert that it visited some nodes
 
 
 def test_searchAStar():
     astar = AStarBestFirstSearch(apla, apla.available_heuristics["goal_count"])
-    res = astar.search()            # Goal, computation_time, opened_nodes(in this order)
-    assert res[1] != 0              # Assert that it took time to compute
-    assert res[-1] > 0              # Assert that it visited at least one node
+    res = astar.search()  # Goal, computation_time, opened_nodes(in this order)
+    assert res[1] != 0  # Assert that it took time to compute
+    assert res[-1] > 0  # Assert that it visited at least one node
